e4034e4deb365369cf2d221a62ca20386bebb844
CNLohr <lohr85@gmail.com>
2023-07-30 09:28:01 -0400

    Update Windows side of custom USB device.

--- demo_custom_device/testtop/testtop.c
+++ demo_custom_device/testtop/testtop.c
@@ -33,19 +33,19 @@ int main()
 		r = hid_send_feature_report( hd, buffer0, sizeof(buffer0) );
 		if( r != sizeof(buffer0) )
 		{
-			fprintf( stderr, "Warning: HID Send fault. Retrying\n" );
+			fprintf( stderr, "Warning: HID Send fault (%d) Retrying\n", r );
 			goto retrysend;
 		}
 		printf( "<" ); // Print this out meaning we sent the data.
 
+		memset( buffer1, 0xff, sizeof( buffer1 ) );
 		buffer1[0] = 0xaa; // First byte must be ID.
-
 
 		r = hid_get_feature_report( hd, buffer1, sizeof(buffer1) );
 
 		printf( ">" ); fflush( stdout);
 
-		if( r != sizeof( buffer1 ) ) { printf( "Got %d\n", r ); break; }
+		if( r != sizeof( buffer1 ) && r != sizeof( buffer1 ) + 1) { printf( "Got %d\n", r ); break; }
 
 		// Validate the scratches matched.
 		if( memcmp( buffer0, buffer1, sizeof( buffer0 ) ) != 0 ) 
