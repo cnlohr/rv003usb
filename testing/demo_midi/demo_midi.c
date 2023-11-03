061e7a1ee9492a40fa59a69203e9d084da0ddb0d
mitxela <mitxela@mitxela.com>
2023-11-03 11:02:54 +0000

    MIDI out to square wave on PC0 and PC1

--- testing/demo_midi/demo_midi.c
+++ testing/demo_midi/demo_midi.c
@@ -11,23 +11,91 @@ typedef struct {
 midi_message_t midi_in;
 
 // midi data from device -> host (IN)
-void midi_send(uint8_t * msg, uint8_t len){
+void midi_send(uint8_t * msg, uint8_t len)
+{
 	memcpy( midi_in.buffer, msg, len );
 	midi_in.len = len;
 }
 #define midi_send_ready() (midi_in.len == 0)
 
+// Output a variable-period square wave on PC0 and PC1
+void tim2_init( void )
+{
+	// Enable GPIOC and TIM2
+	RCC->APB1PCENR |= RCC_APB1Periph_TIM2;
+	RCC->APB2PCENR |= RCC_APB2Periph_GPIOC;
+
+	// PC0 is T2CH3, 10MHz Output alt func, push-pull
+	GPIOC->CFGLR &= ~(0xf<<(4*0));
+	GPIOC->CFGLR |= (GPIO_Speed_10MHz | GPIO_CNF_OUT_PP_AF)<<(4*0);
+
+	// PC1 is T2CH1 alternate
+	GPIOC->CFGLR &= ~(0xf<<(4*1));
+	GPIOC->CFGLR |= (GPIO_Speed_10MHz | GPIO_CNF_OUT_PP_AF)<<(4*1);
+
+	// Provide clock to AFIO peripheral
+	RCC->APB2PCENR |= RCC_APB2Periph_AFIO;
+	// Remap T2CH1 to PC1, leave T2CH3 as C0
+	AFIO->PCFR1 |= AFIO_PCFR1_TIM2_REMAP_PARTIALREMAP2;
+
+	// Set prescaler
+	TIM2->PSC = 0x0008;
+
+	// CTLR1: default is up, events generated, edge align
+	// enable auto-reload of preload
+	TIM2->CTLR1 |= TIM_ARPE;
+
+	// Set TIM2CH1 to output, flip mode, preload enable 
+	TIM2->CHCTLR1 |= TIM_OC1M_1 | TIM_OC1M_0 | TIM_OC1PE; 
+
+	// same for TIM2CH3
+	TIM2->CHCTLR2 |= TIM_OC3M_1 | TIM_OC3M_0 | TIM_OC3PE; 
+
+	// Enable Channel outputs, set CC1 to inverted polarity
+	TIM2->CCER |= TIM_CC1E | TIM_CC3E | TIM_CC1P;
+
+	// Compare/capture registers to zero
+	TIM2->CH1CVR = 0;
+	TIM2->CH3CVR = 0;
+
+}
+
+void tim2_set_period(uint16_t period)
+{
+	if (period) {
+		// Auto reload value
+		TIM2->ATRLR = period;
+		// Timer enable
+		TIM2->CTLR1 |= TIM_CEN;
+	} else {
+		TIM2->CTLR1 &= ~TIM_CEN;
+	}
+}
+
+// 2**( 16 - n/12 )
+const uint16_t noteLookup[128] = { 65535,61856,58385,55108,52015,49096,46340,43739,41284,38967,36780,34716,32767,30928,29192,27554,26007,24548,23170,21870,20642,19484,18390,17358,16384,15464,14596,13777,13004,12274,11585,10935,10321,9742,9195,8679,8192,7732,7298,6888,6502,6137,5792,5467,5161,4871,4598,4339,4096,3866,3649,3444,3251,3068,2896,2734,2580,2435,2299,2170,2048,1933,1825,1722,1625,1534,1448,1367,1290,1218,1149,1085,1024,967,912,861,813,767,724,683,645,609,575,542,512,483,456,431,406,384,362,342,323,304,287,271,256,242,228,215,203,192,181,171,161,152,144,136,128,121,114,108,102,96,91,85,81,76,72,68,64,60,57,54,51,48,45,43 };
+
 // midi data from host -> device (OUT)
-void midi_receive(uint8_t * msg){
+void midi_receive(uint8_t * msg)
+{
+	static uint8_t note;
 
-	printf("Recv %02x %02x %02x %02x\n", msg[0], msg[1], msg[2], msg[3]);
+	if (msg[1] == 0x90 && msg[3] !=0) { // Note-on, velocity nonzero
+		note = msg[2];
+		tim2_set_period( noteLookup[ note ] );
+	}
 
+	// Note-off or note-on with zero velocity
+	else if (msg[2] == note && (msg[1]==0x80 || (msg[1]==0x90 && msg[3]==0)) ) {
+		tim2_set_period(0);
+	}
 }
 
 
 int main()
 {
 	SystemInit();
+	tim2_init();
 	usb_setup();
 	while(1)
 	{
