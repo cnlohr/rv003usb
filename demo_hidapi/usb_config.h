0ae52778d625b40c71a5a45b67c26ff1a89c9eb7
cnlohr <lohr85@gmail.com>
2024-11-11 01:40:27 -0800

    fix note for hidapi device class type

--- demo_hidapi/usb_config.h
+++ demo_hidapi/usb_config.h
@@ -72,7 +72,7 @@ static const uint8_t config_descriptor[] = {
 	0x80,					// bmAttributes (was 0xa0)
 	0x64,					// bMaxPower (200mA)
 
-	//Joystick  (It is unusual that this would be here)
+	//class FF device
 	9,					// bLength
 	4,					// bDescriptorType
 	0,		            // bInterfaceNumber  = 1 instead of 0 -- well make it second.
