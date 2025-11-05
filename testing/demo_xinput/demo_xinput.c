75c5f1f89d9f171bd6c1d18c7a87c143e9739879
meganukebmp <kristiana2000@abv.bg>
2025-11-05 12:16:14 +0100

    Testing - Implement vibration out endpoint handling

--- testing/demo_xinput/demo_xinput.c
+++ testing/demo_xinput/demo_xinput.c
@@ -19,18 +19,30 @@ int main()
 	};
 }
 
+void usb_handle_user_data( struct usb_endpoint * e, int endp, uint8_t * data, int len, struct rv003usb_internal * ist )
+{
+	// rumble report
+	if (endp == 1) {
+		uint8_t bReportID	= data[0]; // 0 for rumble
+		uint8_t bSize		= data[1]; // 8 bytes for rumble
+		uint8_t bType		= data[2]; // must be 0 for rumble
+		uint8_t motor1		= data[3]; // rumble level for motor 1
+		uint8_t motor2		= data[4]; // rumble level for motor 2
+		// 5-7 reserved
+		LogUEvent(12334, motor1, motor2, 0);
+	}
+}
+
 void usb_handle_other_control_message( struct usb_endpoint * e, struct usb_urb * s, struct rv003usb_internal * ist )
 {
 	LogUEvent( SysTick->CNT, s->wRequestTypeLSBRequestMSB, s->lValueLSBIndexMSB, s->wLength );
 }
-
 
 uint8_t off = 0;
 static uint8_t tsajoystick[20] = { 0x00, 0x14, 0x00, 0x00, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
 
 void usb_handle_user_in_request( struct usb_endpoint * e, uint8_t * scratchpad, int endp, uint32_t sendtok, struct rv003usb_internal * ist )
 {
-	LogUEvent(endp, 0, 0, off);
 	if(endp == 1)
 	{
 
@@ -59,6 +71,7 @@ void usb_handle_user_in_request( struct usb_endpoint * e, uint8_t * scratchpad, 
 	}
 	else
 	{
+		LogUEvent(9999, endp, off, 0);
 		// If it's a control transfer, nak it.
 		usb_send_empty( sendtok );
 	}
